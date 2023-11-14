44a75dc9420c86d924a820ed6d82563e69ae8eee
11220080-MochshadamJ <150763038+11220080-MochshadamJ@users.noreply.github.com>
2023-11-14 10:26:15 +0700

    Add files via upload

---
+++ cv.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+    <title>Mochamad Shadam Jaelani - 11220080 - Latihan 7</title>
+</head>
+<body>
+<table border='1' width='100%' height='100%'>
+<tr colspan='2' valign='center'>
+    <td colspan='2' height='20%'>
+    <img src='head.jpg' width='100%'> 
+</td>
+</tr>
+<tr>
+    <td width="20%" valign="top" style="background-image: url('bg3.jpg');">
+    <table width="100%" border="5">
+    <center>
+    <tr><th>Menu</th><tr>
+    <tr><td height="30"><center><a href="index.php">Beranda</a></td></tr>
+    <tr><td height="30"><center><a href="cv.php">CV</a></li></td></tr>
+    <tr><td height="30"><center><a href="galeri.php">Galeri</a></td></tr>
+    </table> </td>
+    <td> <img src='cv.jpg' width='100%'> </b></td>
+</tr>
+<tr>
+    <td colspan='2' valign='middle' align='center' bgcolor='orange'>
+    <i><b> Copyright @2023 - Mochamad Shadam J</td>
+</tr>
+</table>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+    <title>Mochamad Shadam Jaelani - 11220080 - Latihan 7</title>
+</head>
+<body>
+<table border='1' width='100%' height='100%'>
+<tr colspan='2' valign='center'>
+    <td colspan='2' height='20%'>
+    <img src='head.jpg' width='100%'> 
+</td>
+</tr>
+<tr>
+    <td width="20%" valign="top" style="background-image: url('bg5.jpg');">
+    <table width="100%" border="5">
+    <center>
+    <tr><th>Menu</th><tr>
+    <tr><td height="30"><center><a href="index.php">Beranda</a></td></tr>
+    <tr><td height="30"><center><a href="cv.php">CV</a></li></td></tr>
+    <tr><td height="30"><center><a href="galeri.php">Galeri</a></td></tr>
+    </table> </td>
+<td valign='middle' align='center' style="background-image: url('bg.png');"><b> SELAMAT DATANG DI WEB SAYA </b><br>
+<img src='wc.gif'>
+</td>
+</tr>
+<tr>
+    <td colspan='2' valign='middle' align='center' bgcolor='orange'>
+        <i><b> Copyright @2023 - Mochamad Shadam J </td>
+</tr>
+</table>
+</body>
+</html>
